a2067967f1e46992203de2f6469bd61560ff411a
NicoCLM <nc090202@gmail.com>
2024-11-20 23:04:41 -0500

    Updated endpoints

--- backend/src/main/java/co/purrfecthaven/nico/config/WebCorsConfig.java
+++ backend/src/main/java/co/purrfecthaven/nico/config/WebCorsConfig.java
@@ -13,7 +13,7 @@ public class WebCorsConfig implements WebMvcConfigurer{
     
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080")
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(false);
     }
